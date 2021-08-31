6295755e4d261405e845cf275ab406d61a399818
andreicampigotto <andreicampigotto@gmail.com>
2021-08-31 11:16:34 -0300

    ajustado o pullFragment

--- Project_002_GitHub/app/src/main/java/com/proway/a002_git_project/view/PullFragment.kt
+++ Project_002_GitHub/app/src/main/java/com/proway/a002_git_project/view/PullFragment.kt
@@ -1,7 +1,15 @@
 package com.proway.a002_git_project.view
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.proway.a002_git_project.R
+import com.proway.a002_git_project.adapter.PullsAdapter
+import com.proway.a002_git_project.databinding.PullsFragmentBinding
+import com.proway.a002_git_project.model.Pull
+import com.proway.a002_git_project.model.Repository
+import com.proway.a002_git_project.view_model.PullViewModel
+import java.util.*
 
 class PullFragment: Fragment(R.layout.pulls_fragment) {
 
@@ -9,5 +17,14 @@ class PullFragment: Fragment(R.layout.pulls_fragment) {
         fun newInstance() = PullFragment()
     }
 
+    private lateinit var viewModel: PullViewModel
+    private lateinit var binding: PullsFragmentBinding
+    private var adapter = PullsAdapter()
 
+//    private val observerPull = Observer<List<Pull>> {
+//    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
