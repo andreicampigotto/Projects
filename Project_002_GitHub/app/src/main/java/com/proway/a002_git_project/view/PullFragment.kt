1f04213e4568a8cf95b6ca08db17b5b7d6abbac4
andreicampigotto <andreicampigotto@gmail.com>
2021-08-31 11:21:51 -0300

    feito os metodos no onCreate no PullFragment

--- Project_002_GitHub/app/src/main/java/com/proway/a002_git_project/view/PullFragment.kt
+++ Project_002_GitHub/app/src/main/java/com/proway/a002_git_project/view/PullFragment.kt
@@ -3,12 +3,16 @@ package com.proway.a002_git_project.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.proway.a002_git_project.R
 import com.proway.a002_git_project.adapter.PullsAdapter
 import com.proway.a002_git_project.databinding.PullsFragmentBinding
 import com.proway.a002_git_project.model.Pull
 import com.proway.a002_git_project.model.Repository
 import com.proway.a002_git_project.view_model.PullViewModel
+import com.proway.a002_git_project.view_model.RepositoryViewModel
 import java.util.*
 
 class PullFragment: Fragment(R.layout.pulls_fragment) {
@@ -26,5 +30,15 @@ class PullFragment: Fragment(R.layout.pulls_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding = PullsFragmentBinding.bind(view)
+        viewModel = ViewModelProvider(this).get(PullViewModel::class.java)
+
+        binding.pullRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.pullRecyclerView.adapter = adapter
+
+        //viewModel.pulls.observe(viewLifecycleOwner, observerPull)
+
+        viewModel.getPullsList()
     }
 }
