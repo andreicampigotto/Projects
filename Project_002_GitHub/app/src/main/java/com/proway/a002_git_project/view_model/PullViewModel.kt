f64851a2505c92b0635b47ba669779c0f1ded59d
andreicampigotto <andreicampigotto@gmail.com>
2021-08-30 23:24:53 -0300

    criacao do pull view model

--- Project_002_GitHub/app/src/main/java/com/proway/a002_git_project/view_model/PullViewModel.kt
+++ Project_002_GitHub/app/src/main/java/com/proway/a002_git_project/view_model/PullViewModel.kt
@@ -1,4 +1,32 @@
 package com.proway.a002_git_project.view_model
 
-class PullViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.proway.a002_git_project.model.Pull
+import com.proway.a002_git_project.model.Repository
+import com.proway.a002_git_project.repository.GitRepository
+
+class PullViewModel: ViewModel() {
+
+    private val _pulls = MutableLiveData<List<Pull>>()
+    val pulls: LiveData<List<Pull>> = _pulls
+
+    val _error = MutableLiveData<String>()
+    val error : LiveData<String> = _error
+
+    private val gitRepository= GitRepository()
+
+    fun getPullsList() {
+        gitRepository.getRepositoriesList() { list, error ->
+            list?.apply {
+                _pulls.value = this
+            }
+
+            error?.apply {
+                _error.value = this
+            }
+        }
+    }
+} {
 }
