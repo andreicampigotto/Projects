ead7fa972ea835c783f4998a644021023a8c94c5
andreicampigotto <andreicampigotto@gmail.com>
2021-08-31 11:36:24 -0300

    ajustado o Observer pullFragment

--- Project_002_GitHub/app/src/main/java/com/proway/a002_git_project/view/PullFragment.kt
+++ Project_002_GitHub/app/src/main/java/com/proway/a002_git_project/view/PullFragment.kt
@@ -3,17 +3,14 @@ package com.proway.a002_git_project.view
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.proway.a002_git_project.R
 import com.proway.a002_git_project.adapter.PullsAdapter
 import com.proway.a002_git_project.databinding.PullsFragmentBinding
 import com.proway.a002_git_project.model.Pull
-import com.proway.a002_git_project.model.Repository
 import com.proway.a002_git_project.view_model.PullViewModel
-import com.proway.a002_git_project.view_model.RepositoryViewModel
-import java.util.*
 
 class PullFragment: Fragment(R.layout.pulls_fragment) {
 
@@ -25,8 +22,9 @@ class PullFragment: Fragment(R.layout.pulls_fragment) {
     private lateinit var binding: PullsFragmentBinding
     private var adapter = PullsAdapter()
 
-//    private val observerPull = Observer<List<Pull>> {
-//    }
+    private val observerPull = Observer<List<Pull>> { pull ->
+        adapter.update(pull)
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -37,7 +35,7 @@ class PullFragment: Fragment(R.layout.pulls_fragment) {
         binding.pullRecyclerView.layoutManager = LinearLayoutManager(requireContext())
         binding.pullRecyclerView.adapter = adapter
 
-        //viewModel.pulls.observe(viewLifecycleOwner, observerPull)
+        viewModel.pulls.observe(viewLifecycleOwner, observerPull)
 
         viewModel.getPullsList()
     }
