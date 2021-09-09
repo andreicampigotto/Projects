495c271f4bcaa1e09a912090d13e95dbda82d3f4
Andrei Campigotto <38699529+andreicampigotto@users.noreply.github.com>
2021-09-09 11:05:58 -0300

    commitao

--- Crudizin_nada_basico/app/src/main/java/com/proway/crudizin_nada_basico/adapter/SpecialityAdapter.kt
+++ Crudizin_nada_basico/app/src/main/java/com/proway/crudizin_nada_basico/adapter/SpecialityAdapter.kt
@@ -1,7 +1,5 @@
 package com.proway.crudizin_nada_basico.adapter
 
-
-
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
--- Crudizin_nada_basico/app/src/main/java/com/proway/crudizin_nada_basico/database/dao/PatientDao.kt
+++ Crudizin_nada_basico/app/src/main/java/com/proway/crudizin_nada_basico/database/dao/PatientDao.kt
@@ -3,11 +3,15 @@ package com.proway.crudizin_nada_basico.database.dao
 import androidx.room.*
 import androidx.room.OnConflictStrategy.ABORT
 import com.proway.crudizin_nada_basico.model.Patient
+import com.proway.crudizin_nada_basico.model.Speciality
 
 @Dao
 interface PatientDao {
     @Query("Select * from Patient order by name_patient")
     fun fetch(): List<Patient>
+
+    @Query("Select * from Patient where pat_id = :id")
+    fun fetch(id: Int): Speciality
 
     @Query("Select * from Patient where gender = :gender")
     fun fetch(gender: String): List<Patient>
